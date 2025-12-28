bcde519cdbee7ab230da1cc0cadb85514ea66409
Shreyash Tijare <81917827+shreyashtijare@users.noreply.github.com>
2025-12-29 00:32:44 +0530

    Add subscriptionKey to maps-config response

--- api/maps-config.js
+++ api/maps-config.js
@@ -3,6 +3,6 @@ export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   
   res.status(200).json({
-    apiKey: process.env.AZURE_MAPS_KEY
+    subscriptionKey: process.env.AZURE_MAPS_SUBSCRIPTION_KEY
   });
 }
