df8ee7af0c19fd2ae9af826fd69882cdc5d4264f
Shreyash Tijare <81917827+shreyashtijare@users.noreply.github.com>
2025-12-28 23:52:14 +0530

    Add files via upload

---
+++ api/maps-config.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  
+  res.status(200).json({
+    apiKey: process.env.AZURE_MAPS_KEY
+  });
+}
