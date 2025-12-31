1a2d52959ed43ade7f5a5be1feac364f5e4a60eb
Shreyash Tijare <81917827+shreyashtijare@users.noreply.github.com>
2026-01-01 00:41:15 +0530

    Update maps-config.js

--- api/maps-config.js
+++ api/maps-config.js
@@ -2,7 +2,9 @@ export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   
+  // Return BOTH clientId and subscriptionKey
   res.status(200).json({
+    clientId: process.env.AZURE_MAPS_CLIENT_ID,
     subscriptionKey: process.env.AZURE_MAPS_SUBSCRIPTION_KEY
   });
 }
